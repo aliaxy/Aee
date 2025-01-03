323081bf67029909c4df88e5528cbd7f58841309
aliaxy <aruvelut00@163.com>
2025-01-03 15:04:08 +0800

    :white_check_mark: Add Router Test

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ func main() {
 	r.GET("/", func(c *aee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Aee</h1>")
 	})
+
 	r.GET("/hello", func(c *aee.Context) {
 		// expect /hello?name=aliaxy
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *aee.Context) {
-		c.JSON(http.StatusOK, aee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *aee.Context) {
+		// expect /hello/aliaxy
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *aee.Context) {
+		c.JSON(http.StatusOK, aee.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
