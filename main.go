d0005a23c34f60bc1470d9a5b4c75f1e80a91f7b
aliaxy <aruvelut00@163.com>
2025-01-03 15:10:38 +0800

    :sparkles: Add Group Router

--- main.go
+++ main.go
@@ -8,23 +8,34 @@ import (
 
 func main() {
 	r := aee.New()
-	r.GET("/", func(c *aee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Aee</h1>")
+	r.GET("/index", func(c *aee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *aee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Aee</h1>")
+		})
 
-	r.GET("/hello", func(c *aee.Context) {
-		// expect /hello?name=aliaxy
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *aee.Context) {
+			// expect /hello?name=aliaxy
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *aee.Context) {
+			// expect /hello/aliaxy
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *aee.Context) {
+			c.JSON(http.StatusOK, aee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *aee.Context) {
-		// expect /hello/aliaxy
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *aee.Context) {
-		c.JSON(http.StatusOK, aee.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
