77f3ba1f9677a86964e3f0775110f23205c39e35
aliaxy <aruvelut00@163.com>
2025-01-03 17:37:00 +0800

    :sparkles: Add Template Support

--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
 	"time"
 
 	"aee"
 )
 
-func onlyForV2() aee.HandlerFunc {
-	return func(c *aee.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := aee.New()
-	r.Use(aee.Logger()) // global middleware
+	r.Use(aee.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "aliaxy", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(c *aee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Aee</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *aee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", aee.H{
+			"title":  "aee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *aee.Context) {
-			// expect /hello/aliaxy
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/date", func(c *aee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", aee.H{
+			"title": "aee",
+			"now":   time.Now(),
 		})
-	}
+	})
 
 	r.Run(":9999")
 }
